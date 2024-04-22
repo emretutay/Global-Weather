5323907b9a902e47c3ecbe60446f7bd5cca3aa7e
Osman Emre Tutay <emretutay@gmail.com>
2024-04-22 20:37:41 +0300

    Commit

---
+++ Backend/Global-Weather/src/main/java/com/example/GlobalWeather/Global/Weather/GlobalWeatherApplication.java
@@ -0,0 +1,13 @@
+package com.example.GlobalWeather.Global.Weather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalWeatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobalWeatherApplication.class, args);
+	}
+
+}
