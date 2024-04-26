305a206dbfe10a19575205d2e7e20cd9e3ded857
Osman Emre Tutay <emretutay@gmail.com>
2024-04-26 23:07:39 +0300

    Route change

--- Frontend/global-weather/src/app/app.routes.ts
+++ Frontend/global-weather/src/app/app.routes.ts
@@ -1,5 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { CityComponent } from './city/city.component';
+import { WeatherDetailComponent } from './weather-detail/weather-detail.component';
+import { CitySumComponent } from './city-sum/city-sum.component';
+import { ForecastComponent } from './forecast/forecast.component';
+
 
 export const routes: Routes = [
     {
@@ -10,5 +15,13 @@ export const routes: Routes = [
     {
         path:'home',
         component:HomeComponent
-    }
+        
+    },
+    {path:'city/:name',component:CityComponent, children : [
+        {path:'weather/:name',component:CitySumComponent},
+        {path:'detail/:name',component:WeatherDetailComponent},
+        {path:'forecast/:name',component:ForecastComponent}
+
+
+    ]}
 ];
