d6f77993895436b1265a3adf141275fe3304cef9
Osman Emre Tutay <emretutay@gmail.com>
2024-04-26 23:05:58 +0300

    Navbar - Added Search

--- Frontend/global-weather/src/app/navbar/navbar.component.ts
+++ Frontend/global-weather/src/app/navbar/navbar.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
-
+import { WeatherResponse,ForecastResponse,Forecastday } from '../weather/weather.model';
+import { WeatherService } from '../weather/weather.service';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,CommonModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
+  providers:[WeatherService]
 })
 export class NavbarComponent {
+  searchText!: string;
+  weather!: WeatherResponse;
+  forecast!: ForecastResponse;
+  forecastdays!: Forecastday[];
+  text:string="";
+
+  constructor(private weatherService: WeatherService,private router: Router, private route: ActivatedRoute,){
+    console.log(this.route);
+  }
+
+  onSubmit(formValue: { text: string }) {
+    
+    
+    
+    
+    this.router.navigate(['city',formValue.text,'weather',formValue.text])
+    
+    console.log('Form submitted!');
+  }
+
 
 }
