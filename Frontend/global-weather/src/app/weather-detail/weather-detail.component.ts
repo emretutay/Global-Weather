41d61ac371b431a44b44b6eb86b9933fac006c4a
Osman Emre Tutay <emretutay@gmail.com>
2024-04-26 23:05:21 +0300

    Weather Detail Component

---
+++ Frontend/global-weather/src/app/weather-detail/weather-detail.component.ts
@@ -0,0 +1,42 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { WeatherResponse,ForecastResponse,Forecastday } from '../weather/weather.model';
+import { WeatherService } from '../weather/weather.service';
+import { ActivatedRoute } from '@angular/router';
+import { Router ,Params} from '@angular/router';
+
+@Component({
+  selector: 'app-weather-detail',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './weather-detail.component.html',
+  styleUrl: './weather-detail.component.css'
+})
+export class WeatherDetailComponent implements OnInit {
+  weather!: WeatherResponse;
+  forecast!: ForecastResponse;
+  forecastdays!: Forecastday[];
+  name:string="";
+  
+  constructor(private weatherService: WeatherService,private route: ActivatedRoute, private router: Router){}
+  ngOnInit(){
+    this.route.params
+      .subscribe(
+        (params: Params) => {
+          this.name = params['name'];
+          this.weatherService.getWeather(this.name).
+          subscribe(wResponse=>{
+            this.weather = wResponse;
+          })
+          this.weatherService.getForecast(this.name).
+        subscribe(fResponse=>{
+          this.forecast = fResponse;
+          this.forecastdays = this.forecast.forecast.forecastday;
+        })
+            
+        }
+      );
+   
+  }
+
+}
