d28cc55b9c94c990fd43bbe38ed4ee145d03faaa
Osman Emre Tutay <emretutay@gmail.com>
2024-04-29 21:10:59 +0300

    Update app.routes.ts

--- Frontend/global-weather/src/app/app.routes.ts
+++ Frontend/global-weather/src/app/app.routes.ts
@@ -4,6 +4,9 @@ import { CityComponent } from './city/city.component';
 import { WeatherDetailComponent } from './weather-detail/weather-detail.component';
 import { CitySumComponent } from './city-sum/city-sum.component';
 import { ForecastComponent } from './forecast/forecast.component';
+import { ForecastHourComponent } from './forecast-hour/forecast-hour.component';
+import { AstroComponent } from './astro/astro.component';
+import { AirQualityComponent } from './air-quality/air-quality.component';
 
 
 export const routes: Routes = [
@@ -20,7 +23,11 @@ export const routes: Routes = [
     {path:'city/:name',component:CityComponent, children : [
         {path:'weather/:name',component:CitySumComponent},
         {path:'detail/:name',component:WeatherDetailComponent},
-        {path:'forecast/:name',component:ForecastComponent}
+        {path:'forecast/:name',component:ForecastComponent},
+        {path:'forecast/:name/:day',component:ForecastHourComponent},
+        {path:'astro/:name',component:AstroComponent},
+        {path:'air/:name',component:AirQualityComponent}
+
 
 
     ]}
